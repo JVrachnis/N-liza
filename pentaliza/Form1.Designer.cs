056e03d1f6ae3a7fc7ef75fb0d941dcc5ed49ec5
johnythefoxGR <john.al.vra@hotmail.com>
2018-02-09 10:46:59 +0200

    added the extra packages and 1 fix

--- pentaliza/Form1.Designer.cs
+++ pentaliza/Form1.Designer.cs
@@ -201,8 +201,6 @@
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Nliza";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.Resize += new System.EventHandler(this.Form1_Resize);
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
